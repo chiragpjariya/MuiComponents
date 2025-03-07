08c7a98445ca0f9d950b8e9a933b7dd1332ce432
chirag jariya <chiragjariya0109@gmail.com>
2025-03-07 11:31:13 +0530

    u

---
+++ inputbox/src/App.jsx
@@ -0,0 +1,11 @@
+import InputBox from "./components/InputBox"
+
+function App() {
+  return (
+    <div className="bg-blue-950 h-screen w-full flex items-center justify-center" >
+      <InputBox />
+    </div>
+  )
+}
+
+export default App
