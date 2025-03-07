b7eaf047fe0672fcdcda05a5f5e602702563700a
chirag jariya <chiragjariya0109@gmail.com>
2025-03-07 14:08:52 +0530

    sucessfully created

--- inputbox/src/components/InputBox.jsx
+++ inputbox/src/components/InputBox.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
-
-function InputBox() {
+const InputBox = () => {
   return (
-    <div>InputBox</div>
-  )
-}
+    <div className="w-[680px]">
+      <div className="relative w-full">
+        <input
+          type="text"
+          required
+          className="peer absolute w-full text-aliceblue outline-none text-[2.2rem] px-[30px] leading-[80px] 
+      z-50 text-white
+      rounded-[10px] border-[2px] border-[#f0ffff] bg-transparent transition-all ease-in duration-200 focus:z-20"
+        />
 
-export default InputBox
+        {/* Floating label */}
+        <div className="absolute px-[30px]  py-[14px] text-[2.2rem] duration-[0.5s] text-white 
+      peer-focus:-translate-y-11 peer-focus:z-50 peer-focus:px-6 peer-focus:bg-transparent 
+      peer-focus:scale-[0.88] peer-focus:-translate-x-1 peer-focus:bg-blue-950 peer-focus:text-[1.8rem]">
+          Enter Your Name
+        </div>
+      </div>
+
+
+    </div>
+  );
+};
+
+export default InputBox;
